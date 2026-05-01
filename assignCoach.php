252eaf8c0b1161b9af1e8150297eefa9940b11f5
Ceral Pigney <calvintyreese11@gmail.com>
2026-05-01 18:24:11 -0500

    assignCoach works

--- assignCoach.php
+++ assignCoach.php
@@ -27,9 +27,39 @@
                     die("Connection failed: ".$conn->connect_error);
                 }
       
-                if(isset($_POST["C_USERID"]) && isset($_POST["TeamID"])) {
+                if(isset($_POST["C_USERID"]) && isset($_POST["TeamID"]) && isset($_POST["Role"])) {
+                    if ($_POST["Role"] == 'Head Coach') {
+                        $sql = "update team set HeadCoach = ".$_POST["C_USERID"]." where TeamID = ".$_POST["TeamID"];
+                        $result = $conn->query($sql);
+                        if (!$result) {
+                            echo("<h4>Some error occurred! <a href=\"assignCoach.php\">Try again!</a>.<h4>");
+                        }
+
+                        $result = $conn->query("Select * from coaches where TeamID =".$_POST["TeamID"]." and Role ='".$_POST["Role"]."'");
+                        // error handling
+                        if(!$result)
+                        {
+                            echo("<h4>Some error occurred! <a href=\"assignCoach.php\">Try again!</a>.<h4>");
+                        }
+
+                        $row = $result->fetch_assoc();
+                        if ($result->num_rows > 0) {
+                            $newres = $conn->query("update coaches set C_USERID =".$_POST["C_USERID"]." where TeamID = ".$row['TeamID']." and C_USERID = ".$row['C_USERID']);
+
+                            if($newres)
+                                {
+                                echo("<h4>Coach ".$_POST["C_USERID"]." successfully added to Team ".$_POST["TeamID"]."<h4>");
+                                echo("<br> Return to <a href=\"seeCoaches.php\">Coach Management</a>.");
+                                }
+                        }
+                    else
+                        {
+                            echo("<h4>Some error occurred! <a href=\"assignCoach.php\">Try again!</a>.<h4>");
+                        }
+
+                    } else {
                     
-                    $sql = "update team set HeadCoach = ".$_POST["C_USERID"]." where TeamID = ".$_POST["TeamID"];
+                    $sql = "insert into coaches values ('".$_POST["TeamID"]."', '".$_POST["C_USERID"]."', '".$_POST["Role"]."')";
                     //Run query
                     $result = $conn->query($sql);
                         
@@ -43,13 +73,17 @@
                             echo("<h4>Some error occurred! <a href=\"assignCoach.php\">Try again!</a>.<h4>");
                         }
                 }
-                else {
+
+                // Add to team form
+                } else {
 
                     echo "<h4> Assign a coach to a team</h4>";
                     ?>
                     <form action="assignCoach.php" method="post">
                         Team ID*: <input type="text" name="TeamID"><br>
                         Coach ID*: <input type="text" name="C_USERID"><br>
+                        Role: <input type="text" name="Role"><br>
+
 
                         <input type="submit"> &nbsp;&nbsp;
                         <input type="reset">
